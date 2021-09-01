8f4fc01a4139c9fad48834d6cad0d77d896ff7f8
mebenny <33872364+mebenny@users.noreply.github.com>
2021-09-01 16:09:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ sed -i 's/192.168.1.1/192.168.10.15/g' package/base-files/files/bin/config_gener
 # sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings  #设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 
 #2. Clear the login password
-# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
@@ -33,7 +33,7 @@ sed -i 's/192.168.1.1/192.168.10.15/g' package/base-files/files/bin/config_gener
 # sed -i 's/OpenWrt /meBenny compiled in $(TZ=UTC-8 date +%Y.%m.%d) @ OpenWrt /g' $ZZZ
 
 # 修改机器名称
-# sed -i "s/meBenny/$device_name/g" package/base-files/files/bin/config_generate
+sed -i "s/meBenny/$device_name/g" package/base-files/files/bin/config_generate
 
 # 修改时区
 # sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
