3c51b7d85575962119afe903a4a29ac5f0a15988
mebenny <33872364+mebenny@users.noreply.github.com>
2021-10-19 15:03:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,7 @@ sed -i 's/192.168.1.1/192.168.10.15/g' package/base-files/files/bin/config_gener
 # sed -i 's/+IPV6:libip6tc//g' package/network/config/firewall/Makefile
 # sed -i 's/+IPV6:kmod-nf-conntrack6//g' package/network/config/firewall/Makefile
 # sed -i 's/+IPV6:libip6tc//g' package/network/utils/iptables/Makefile
+
 # 修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-opentomcat/g" feeds/luci/collections/luci/Makefile
 
@@ -23,26 +24,48 @@ sed -i "s/OpenWrt/Home803/g" package/base-files/files/bin/config_generate
 
 # 修改密码
 sed -i 's/root::0:0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/base-files/files/etc/shadow
+#### 修改想要的root密码
+#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 # 打开NTP
 sed -i "s/'0'/'1'\n   set system.ntp.enable_server='$ntp_name'/g" package/base-files/files/bin/config_generate
 
-# 本地启动脚本
-sed -i "2i net.netfilter.nf_conntrack_max=65535" ./package/base-files/files/etc/sysctl.conf
-sed -i "3i net.ipv6.conf.default.forwarding=2" ./package/base-files/files/etc/sysctl.conf
-sed -i "4i net.ipv6.conf.all.forwarding=2" ./package/base-files/files/etc/sysctl.conf
-sed -i "5i net.ipv6.conf.default.accept_ra=2" ./package/base-files/files/etc/sysctl.conf
-sed -i "6i net.ipv6.conf.all.accept_ra=2" ./package/base-files/files/etc/sysctl.conf
+# Modify default network connect
+echo 'net.netfilter.nf_conntrack_max=65535' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.default.forwarding=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.default.accept_ra=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo ' net.ipv6.conf.all.accept_ra=2' | tee -a package/base-files/files/etc/sysctl.conf
+# sed -i "2i net.netfilter.nf_conntrack_max=65535" ./package/base-files/files/etc/sysctl.conf
+# sed -i "3i net.ipv6.conf.default.forwarding=2" ./package/base-files/files/etc/sysctl.conf
+# sed -i "4i net.ipv6.conf.all.forwarding=2" ./package/base-files/files/etc/sysctl.conf
+# sed -i "5i net.ipv6.conf.default.accept_ra=2" ./package/base-files/files/etc/sysctl.conf
+# sed -i "6i net.ipv6.conf.all.accept_ra=2" ./package/base-files/files/etc/sysctl.conf
 
 #启动脚本插入到 'exit 0' 之前即可随系统启动运行。
 sed -i '3i /etc/init.d/samba stop' package/base-files/files/etc/rc.local #停止samba服务
 sed -i '4i /etc/init.d/samba disable' package/base-files/files/etc/rc.local #禁止samba服务开机自动
 
+# Remove some default packages
+sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
+
 #移除不用软件包
-#rm -rf package/lean/luci-app-dockerman
-#rm -rf package/lean/luci-app-wrtbwmon
-#rm -rf package/lean/adbyby
-#rm -rf package/lean/luci-app-adbyby-plus
+rm -rf package/lean/luci-app-dockerman
+rm -rf package/lean/luci-app-wrtbwmon
+rm -rf package/lean/adbyby
+rm -rf package/lean/luci-app-unblockmusic
+rm -rf package/lean/UnblockNeteaseMusic
+rm -rf package/lean/UnblockNeteaseMusicGo
+rm -rf package/lean/luci-app-jd-dailybonus
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-netdata
+rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-vssr
 
 # 修改时区
 # sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+./scripts/feeds update -a
+./scripts/feeds install -a
