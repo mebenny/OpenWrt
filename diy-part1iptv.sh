478bfe15b0fa201fb2cb7458bdb90dbe23ebbfa4
mebenny <33872364+mebenny@users.noreply.github.com>
2022-01-18 14:43:35 +0800

    Update diy-part1iptv.sh

--- diy-part1iptv.sh
+++ diy-part1iptv.sh
@@ -17,12 +17,12 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git dansir118 https://github.com/dansir118/openwrt-packages' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 # git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/lean/luci-app-autoipsetadder
 # IPTV
+echo 'src-git dansir118 https://github.com/dansir118/openwrt-packages' >>feeds.conf.default
 # git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/luci-app-iptvhelper
 # 获取日志查看器
