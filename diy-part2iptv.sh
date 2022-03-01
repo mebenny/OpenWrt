3cfcd8db528f1bf89a7684a861f04f8d6569a2e4
mebenny <33872364+mebenny@users.noreply.github.com>
2022-03-01 21:15:59 +0800

    Update diy-part2iptv.sh

--- diy-part2iptv.sh
+++ diy-part2iptv.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-echo "修改默认LAN口IP，及相应的局域网设置，详细见diy-part1.sh设置"
-sed -i "/exit 0/i\chmod +x /etc/webweb.sh && source /etc/webweb.sh" $ZZZ
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.77/g' package/base-files/files/bin/config_generate
 # sed -i 's/+IPV6:libip6tc//g' package/network/config/firewall/Makefile
@@ -20,13 +17,20 @@ sed -i 's/192.168.1.1/192.168.10.77/g' package/base-files/files/bin/config_gener
 # sed -i 's/+IPV6:libip6tc//g' package/network/utils/iptables/Makefile
 
 # Modify default NAT
-# export ZZ="package/lean/default-settings/files/zzz-default-settings"
-# sed -i "13i uci set network.lan.proto='static'" $ZZ
-# sed -i "14i uci set network.lan.netmask='255.255.255.0'" $ZZ
-# sed -i "15i uci set network.lan.gateway='192.168.10.1'" $ZZ
-# sed -i "16i uci set network.lan.dns='192.168.10.1'" $ZZ
-# sed -i "17i uci set network.lan.ip6assign='64'" $ZZ
-# sed -i "18i uci commit network\n" $ZZ
+export ZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i "11i uci delete network.wan 
+sed -i "12i uci delete network.wan6
+sed -i "13i uci set network.lan.ifname='eth0'" $ZZ
+sed -i "14i uci set network.lan._orig_ifname 'eth0'" $ZZ
+sed -i "15i uci set network.lan._orig_bridge 'true'" $ZZ
+sed -i "16i uci set network.lan.proto='static'" $ZZ
+sed -i "17i uci set network.lan.ipaddr '192.168.10.77'" $ZZ
+sed -i "18i uci set network.lan.netmask='255.255.255.0'" $ZZ
+sed -i "19i uci set network.lan.gateway='192.168.10.1" $ZZ
+sed -i "20i uci set network.lan.broadcast='192.168.10.255" $ZZ
+sed -i "21i uci set network.lan.dns='192.168.10.1" $ZZ
+sed -i "22i uci set network.lan.delegate='0'" $ZZ
+sed -i "23i uci commit network\n" $ZZ
 
 # 修改默认主题
 sed -i "s/luci-theme-bootstrap/luci-theme-mcat/g" feeds/luci/collections/luci/Makefile
@@ -38,22 +42,20 @@ sed -i "s/OpenWrt/IPTV/g" package/base-files/files/bin/config_generate
 # sed -i 's/root:::0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/base-files/files/etc/shadow
 #### 修改想要的root密码
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
-# 设置密码为空qwe**789
-# sed -i '/J0I9VtO1JT99G4R2iZKaA/d' $ZZZ
 
 # 修改连接数数
 # sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-#修正连接数
+# 修正连接数
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
 
-#启动脚本插入到 'exit 0' 之前即可随系统启动运行。
+# samba服务启动脚本插入到 'exit 0' 之前即可随系统启动运行。
 # sed -i '3i /etc/init.d/samba stop' package/base-files/files/etc/rc.local #停止samba服务
 # sed -i '4i /etc/init.d/samba disable' package/base-files/files/etc/rc.local #禁止samba服务开机自动
 
 #
 # 防火墙自定义
-sed -i "46i echo 'iptables -t nat -I POSTROUTING -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "45i echo 'iptables -t nat -I POSTROUTING -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
 
 # Remove some default packages
 sed -i 's/luci-app-arpbind//g;s/luci-app-turboacc//g;s/luci-app-upnp//g;s/luci-app-ssr-plus//g;s/luci-app-accesscontrol//g;s/luci-app-adbyby-plus//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-qbittorrent//g;s/luci-app-unblockmusic//g;s/luci-app-uugamebooster//g;s/luci-app-vlmcsd//g;s/luci-app-ttyd//g;s/luci-app-xlnetacc//g;s/luci-app-wol//g' include/target.mk
