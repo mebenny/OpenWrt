25124692eb49f3c8fd68c1287bbef597260d06f5
mebenny <33872364+mebenny@users.noreply.github.com>
2022-03-02 23:04:37 +0800

    Update diy-part1.sh

--- home803/diy-part1.sh
+++ home803/diy-part1.sh
@@ -25,7 +25,7 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/luci-app-iptvhelper
 # 获取日志查看器
-git clone https://github.com/gdck/luci-app-tn-logview package/lean/luci-app-tn-logview
+# git clone https://github.com/gdck/luci-app-tn-logview package/lean/luci-app-tn-logview
 # 添加luci-app-advanced
 # git clone https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
 # autotimeset 定时设置插件
