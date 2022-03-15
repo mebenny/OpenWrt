11ad10a1b335acf5e3a1c1009a6dcfbec050e227
mebenny <33872364+mebenny@users.noreply.github.com>
2022-03-15 09:03:11 +0800

    Update diy-part2.sh

--- build/iptv/diy-part2.sh
+++ build/iptv/diy-part2.sh
@@ -45,26 +45,27 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 sed -i "45i echo 'iptables -t nat -I POSTROUTING -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
 
 # Remove some default packages
-sed -i 's/luci-app-arpbind//g;s/luci-app-turboacc//g;s/luci-app-upnp//g;s/luci-app-ssr-plus//g;s/luci-app-adbyby-plus//g;s/luci-app-accesscontrol//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-qbittorrent//g;s/luci-app-unblockmusic//g;s/luci-app-uugamebooster//g;s/luci-app-vlmcsd//g;s/luci-app-ttyd//g;s/luci-app-xlnetacc//g;s/luci-app-wol//g' include/target.mk
+sed -i 's/luci-app-accesscontrol//g;s/luci-app-adbyby-plus//g;s/luci-app-arpbind//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-ssr-plus//g;s/luci-app-ttyd//g;s/luci-app-turboacc//g;s/luci-app-unblockmusic//g;s/luci-app-upnp//g;s/luci-app-vlmcsd//g;s/luci-app-vsftpd//g;s/luci-app-wireguard//g;s/luci-app-wol//g;s/luci-app-xlnetacc//g' include/target.mk
 
 #移除不用软件包
-rm -rf package/luci-app-arpbind
-rm -rf package/lean/luci-app-turboacc
-rm -rf package/lean/luci-app-upnp
-rm -rf package/lean/luci-app-ssr-plus
 rm -rf package/lean/luci-app-accesscontrol
 rm -rf package/lean/luci-app-adbyby-plus
+rm -rf package/lean/luci-app-arpbind
 rm -rf package/lean/luci-app-ddns
 rm -rf package/lean/luci-app-ipsec-vpnd
 rm -rf package/lean/luci-app-nlbwmon
-rm -rf package/lean/luci-app-qbittorrent
-rm -rf package/lean/luci-app-wrtbwmon
-rm -rf package/lean/adbyby
+rm -rf package/lean/luci-app-ssr-plus
+rm -rf package/lean/luci-app-ttyd
+rm -rf package/lean/luci-app-turboacc
 rm -rf package/lean/luci-app-unblockmusic
-rm -rf package/lean/luci-app-uugamebooster
+rm -rf package/lean/luci-app-upnp
 rm -rf package/lean/luci-app-vlmcsd
 rm -rf package/lean/luci-app-vsftpd
+rm -rf package/lean/luci-app-wireguard
+rm -rf package/lean/luci-app-wol
 rm -rf package/lean/luci-app-xlnetacc
+
+
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
