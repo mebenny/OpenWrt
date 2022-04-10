6a0882ce3a66ccce0626c25cab128cbbd4ea6c23
mebenny <33872364+mebenny@users.noreply.github.com>
2022-04-10 20:14:47 +0800

    Update diy-part2.sh

--- build/iptv/diy-part2.sh
+++ build/iptv/diy-part2.sh
@@ -45,7 +45,7 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 sed -i "45i echo 'iptables -t nat -I POSTROUTING -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
 
 # Remove some default packages
-sed -i 's/luci-app-accesscontrol//g;s/luci-app-adbyby-plus//g;s/luci-app-arpbind//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-ssr-plus//g;s/luci-app-ttyd//g;s/luci-app-turboacc//g;s/luci-app-unblockmusic//g;s/luci-app-upnp//g;s/luci-app-vlmcsd//g;s/luci-app-vsftpd//g;s/luci-app-wireguard//g;s/luci-app-wol//g;s/luci-app-xlnetacc//g' include/target.mk
+sed -i 's/luci-app-accesscontrol//g;s/luci-app-adbyby-plus//g;s/luci-app-arpbind//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-ssr-plus//g;s/luci-app-ttyd//g;s/luci-app-turboacc//g;s/luci-app-unblockmusic//g;s/luci-app-upnp//g;s/luci-app-vlmcsd//g;s/luci-app-vsftpd//g;s/luci-app-wireguard//g;s/luci-app-xlnetacc//g' include/target.mk
 
 #移除不用软件包
 rm -rf package/lean/luci-app-accesscontrol
@@ -62,7 +62,7 @@ rm -rf package/lean/luci-app-upnp
 rm -rf package/lean/luci-app-vlmcsd
 rm -rf package/lean/luci-app-vsftpd
 rm -rf package/lean/luci-app-wireguard
-rm -rf package/lean/luci-app-wol
+# rm -rf package/lean/luci-app-wol
 rm -rf package/lean/luci-app-xlnetacc
 
 
