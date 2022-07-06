0d1420321bdefb2f271d44c08d13140f9ef007c4
mebenny <33872364+mebenny@users.noreply.github.com>
2022-07-06 22:28:59 +0800

    Update diy-part1.sh

--- home803/diy-part1.sh
+++ home803/diy-part1.sh
@@ -15,10 +15,15 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+
 # echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
 # git clone https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 # git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/lean/luci-app-autoipsetadder
 # IPTV
@@ -30,3 +35,4 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # git clone https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
 # autotimeset 定时设置插件
 git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci-app-autotimeset
+git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-theme-mcat.git package/lean/luci-app-autotimeset
