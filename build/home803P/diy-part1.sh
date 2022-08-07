4898228cdc88cdff6c396b84f7b7e0adb9cf50f8
bj803 <109253859+bj803@users.noreply.github.com>
2022-08-07 12:30:44 +0800

    Add files via upload

---
+++ build/home803P/diy-part1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# git clone https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+# git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/lean/luci-app-autoipsetadder
+# IPTV
+# git clone https://github.com/riverscn/luci-app-omcproxy.git package/lean/luci-app-omcproxy
+# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/luci-app-iptvhelper
+# 获取日志查看器
+# git clone https://github.com/gdck/luci-app-tn-logview package/lean/luci-app-tn-logview
+# 添加luci-app-advanced
+# git clone https://github.com/sirpdboy/luci-app-advanced package/lean/luci-app-advanced
+
+#sirpdboy
+# autotimeset 定时设置插件
+git clone https://github.com/sirpdboy/luci-app-autotimeset package/lean/luci-app-autotimeset
+
---
+++ build/home803P/diy-part2.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/+IPV6:libip6tc//g' package/network/config/firewall/Makefile
+# sed -i 's/+IPV6:kmod-nf-conntrack6//g' package/network/config/firewall/Makefile
+# sed -i 's/+IPV6:libip6tc//g' package/network/utils/iptables/Makefile
+
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+
+# 修改机器名称
+sed -i "s/OpenWrt/Home803/g" package/base-files/files/bin/config_generate
+
+# 修改密码
+# sed -i 's/root:::0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/base-files/files/etc/shadow
+#### 修改想要的root密码
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$qTM.tEk0$J0I9VtO1JT99G4R2iZKaA.:18858:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+# 打开NTP
+sed -i "3i uci set system.ntp.enable_server='1'" package/lean/default-settings/files/zzz-default-settings
+# sed -i "s/system.ntp.enable_server='0'/system.ntp.enable_server='1'/g" package/base-files/files/bin/config_generate
+
+# 修改连接数数
+# sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+#修正连接数
+# sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
+
+# Modify default network connect
+echo 'net.netfilter.nf_conntrack_max=65535' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.default.forwarding=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.default.accept_ra=2' | tee -a package/base-files/files/etc/sysctl.conf
+echo 'net.ipv6.conf.all.accept_ra=2' | tee -a package/base-files/files/etc/sysctl.conf
+
+# 防火墙自定义规则
+sed -i "45i echo 'WAN6=eth3' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "46i echo 'LAN=br-lan br-lan10 br-lan11' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "47i echo 'ip6tables -t nat -A POSTROUTING -o $WAN6 -j MASQUERADE' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "48i echo 'ip6tables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+sed -i "49i echo 'ip6tables -A FORWARD -i $LAN -j ACCEPT' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
+
+#启动脚本插入到 'exit 0' 之前即可随系统启动运行。
+# sed -i '3i /etc/init.d/samba stop' package/base-files/files/etc/rc.local #停止samba服务
+# sed -i '4i /etc/init.d/samba disable' package/base-files/files/etc/rc.local #禁止samba服务开机自动
+
+# Remove some default packages
+sed -i 's/luci-app-accesscontrol//g;s/luci-app-adbyby-plus//g;s/luci-app-ddns//g;s/luci-app-ipsec-vpnd//g;s/luci-app-nlbwmon//g;s/luci-app-ssr-plus//g;s/luci-app-ttyd//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-vsftpd//g;s/luci-app-wireguard//g;s/luci-app-xlnetacc//g;s/luci-app-zerotier//g' include/target.mk
+
+#移除不用软件包
+rm -rf package/lean/luci-app-accesscontrol
+rm -rf package/lean/luci-app-adbyby-plus
+rm -rf package/lean/luci-app-ddns
+rm -rf package/lean/luci-app-ipsec-vpnd
+rm -rf package/lean/luci-app-nlbwmon
+rm -rf package/lean/luci-app-ssr-plus
+rm -rf package/lean/luci-app-ttyd
+rm -rf package/lean/luci-app-unblockmusic
+rm -rf package/lean/luci-app-vlmcsd
+rm -rf package/lean/luci-app-vsftpd
+rm -rf package/lean/luci-app-wireguard
+# rm -rf package/lean/luci-app-wol
+rm -rf package/lean/luci-app-xlnetacc
+rm -rf package/lean/luci-app-zerotier
+
+
+
+./scripts/feeds update -a
+./scripts/feeds install -a
